99bd8d957f11fb605a39f4206d5b36276d43d889
David Aryasetia <david.aryasetia@gmail.com>
2024-02-13 23:53:13 +0700

    Add Bash 5 Program

---
+++ bash_five_program/five.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Program to run my other four programs
+
+./questionnaire.sh
+./countdown.sh 3ex
+./bingo.sh
+./fortune.sh
---
+++ bash_five_program/fortune.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
+
+function GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
+    echo Ask a yes or no question:
+  else
+    echo Try again. Make sure it ends with a question mark:
+  fi
+
+  read QUESTION
+}
+
+GET_FORTUNE
+until [[ $QUESTION =~ \?$ ]]
+do
+
+  GET_FORTUNE again
+done
+
+echo -e "\n${RESPONSES[$N]}"
